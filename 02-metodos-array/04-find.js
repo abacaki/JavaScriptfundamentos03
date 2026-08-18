9e55ca71cddc9ea591f21f1b16e2822f3fb48d92
abacaki <abacaki98@gmail.com>
2026-08-18 14:03:00 -0300

    Adicionando método find: 02-metodos-array\find.js

---
+++ 02-metodos-array/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Guilherme", "Thiago", "Daniel", "Guedes"];
+
+const aluno = alunos.find((nome) => nome === "Thiago");
+
+console.log(aluno);
+
+const aluno1 = alunos.find((nome) => nome === "Benício");
+
+console.log(aluno1)
