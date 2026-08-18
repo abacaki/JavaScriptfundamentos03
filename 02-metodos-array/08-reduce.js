5e841690d568cea97515c6f9080eb0d80e62b524
abacaki <abacaki98@gmail.com>
2026-08-18 14:34:52 -0300

    Adicionando método reduce: 02-metodos-array\08-reduce.js

--- 02-metodos-array/08-reduce.js
+++ 02-metodos-array/08-reduce.js
@@ -3,3 +3,14 @@ const valores = [10, 20, 30, 40, 50];
 const total = valores.reduce((acumulador, valor) => acumulador + valor, 0);
 
 console.log(total);
+// 0 + 10 = 10
+// 10 + 20 = 30
+// 30 + 30 = 60
+// 60 + 40 = 100
+// 100 + 50 = 50
+
+//map() transforma cada item
+//filter () filtra item
+//some() algum atende?
+//every() todos atendem?
+//reduce() acumula tudo em um resultado
