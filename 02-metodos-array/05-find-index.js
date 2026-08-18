17f2d3d98ef65d6ab58ed0b4f8e277d49cf5b5c7
abacaki <abacaki98@gmail.com>
2026-08-18 14:13:44 -0300

    Adicionando método findIndex: 02-metodos-array\05-find-index.js

---
+++ 02-metodos-array/05-find-index.js
@@ -0,0 +1,9 @@
+let nomes =["Bueno", "Sabrina", "Julio", "Alponte"];
+
+const indice = nomes.findIndex((n) => n === "Julio");
+
+console.log(indice);
+
+const indice1 = nomes.findIndex((n) => n === "Mikaele");
+
+console.log(indice1);
